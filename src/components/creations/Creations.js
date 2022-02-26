58b3ce54697024b804a42210ba32f937c257ce72
JacobTTorres <jacobttorres@hotmail.com>
2022-02-26 11:44:39 -0600

    added creations component

---
+++ src/components/creations/Creations.js
@@ -0,0 +1,9 @@
+import "./creations.css";
+
+export default function Creations() {
+  return (
+    <section className="creationContainer">
+      <div className="creationContent">creation component</div>
+    </section>
+  );
+}
